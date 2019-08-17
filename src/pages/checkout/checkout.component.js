1a2ab0d04e2822e26981e27ad0b8ee9e977f56fd
Gustavo Henrique de Lima Perenciolo <gustavolima@lnxcit020062.cit>
2019-08-17 14:19:33 -0300

    Add payments ability

--- src/pages/checkout/checkout.component.js
+++ src/pages/checkout/checkout.component.js
@@ -8,6 +8,7 @@ import {
   selectCartTotal
 } from '../../redux/cart/cart.selectors';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -34,6 +35,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>Total: ${total}</span>
     </div>
+    <div className="warning-info">
+      *For tests use card number 4242 4242 4242 4242
+    </div>
+    <div className="warning-info">*Exp: 01/21</div>
+    <div className="warning-info">*CVV: 123</div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 const mapStateToProps = createStructuredSelector({
